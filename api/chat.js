368cdba41ae7ce879ce22f48a6a060f172c171c4
josesesse <jose.ses.se@gmail.com>
2026-08-03 00:03:45 +0200

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
+
+  const apiKey = process.env.GEMINI_API_KEY;
+
   res.status(200).json({
-    ok: true,
-    mensaje: "Servidor funcionando"
+    encontrada: !!apiKey,
+    longitud: apiKey ? apiKey.length : 0
   });
+
 }
