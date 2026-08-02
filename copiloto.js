f2f7140048ff578051774f9af9bc21211f628483
josesesse <jose.ses.se@gmail.com>
2026-08-03 00:42:21 +0200

    copiloto.js

--- copiloto.js
+++ copiloto.js
@@ -1,7 +1,47 @@
 window.Copiloto = {
 
-    abrir() {
-        alert("🚗 Copiloto conectado");
-    }
+  modal: null,
+
+  init() {
+
+    this.modal = document.createElement("div");
+    this.modal.id = "copiloto-modal";
+
+    this.modal.innerHTML = `
+      <div class="cp-backdrop">
+        <div class="cp-window">
+
+          <div class="cp-header">
+            🦁 Copiloto
+            <button id="cp-close">✕</button>
+          </div>
+
+          <div class="cp-body">
+
+            Próximamente...
+
+          </div>
+
+        </div>
+      </div>
+    `;
+
+    this.modal.style.display = "none";
+
+    document.body.appendChild(this.modal);
+
+    document.getElementById("cp-close").onclick = () => this.cerrar();
+
+  },
+
+  abrir() {
+    this.modal.style.display = "block";
+  },
+
+  cerrar() {
+    this.modal.style.display = "none";
+  }
 
 };
+
+window.addEventListener("load", () => Copiloto.init());
